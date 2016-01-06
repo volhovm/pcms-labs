51308cf2af2218c318e0a659cef0e90e739ad9eb
Mikhail Volkhov <volhovm.cs@gmail.com>
2016-01-06 15:46:54 +0300

    geometry: fixed J

--- computational_geometry/J/J.cpp
+++ computational_geometry/J/J.cpp
@@ -8,6 +8,8 @@
 #include <set>
 using namespace std;
 
+// OK
+
 struct point {
 public:
     long double x, y;
@@ -58,50 +60,11 @@ ostream& operator<<(ostream& os, const point& p) {
 
 long double distance(point a, point b) { return (b - a).norm(); }
 
-inline int turn(point a, point b) {
-    long double turnvalue = ((a.x * b.y) -
-                             (a.y * b.x));
-    return turnvalue > 0 ? 1 : turnvalue < 0 ? -1 : 0;
-}
-
-inline int turn(point a, point b, point c, point d) {
-    return turn(b - a, d - c);
-}
-
-inline int turn(point a, point b, point c) {
-    return turn (a, b, a, c);
-}
-
-// line intersection
-point intersection(point a, point b, point c, point d) {
-    // if b - a || d - c then fail(((
-    {
-        point x = b - a;
-        point y = d - c;
-        if (x/x.norm() != y/y.norm()) { cout << "LOL FAIL" << endl; exit(1); }
-    }
-    // like this shit a lot
-    long double x = (d.y-a.y+(a.x/b.x)-(d.x/c.x)) /
-                 ((b.y-a.y)/(b.x-a.x)-(c.y-b.y)/(c.x-d.x));
-    long double y = (b.y-a.y)*(x-a.x)/(b.x-a.x)+a.y;
-    return point(x,y);
-}
-
 struct segment {
     point start, end;
     segment (point a, point b) : start(a), end(b) {};
     point middle() {
-        cout << " middle0: " << start << " " << end << endl;
-        cout << " middle1: " << start.x/2.0 << " " << end.x/2.0 << endl;
-        long double x0 = start.x/2 + end.x/2;
-        long double y0 = start.y/2 + end.y/2;
-        cout << " middle2: " << x0 << " " << y0 << endl;
-        return point(x0, y0);
-    }
-    // that's a normal that satisfies `turn (end - start) (normal) > 0`
-    point normal() {
-        point b0 = end - start;
-        return point(-b0.y, b0.x);
+        return point((start.x+end.x)/2, (start.y+end.y)/2);
     }
 };
 
@@ -124,26 +87,14 @@ ostream& operator<<(ostream& os, const circle& p) {
     return os;
 };
 
-    // http://www.abecedarical.com/zenosamples/zs_circle3pts.html
+// http://www.abecedarical.com/zenosamples/zs_circle3pts.html
 circle from_3_points(point a, point b, point c) {
-    // this should work, but slow and cumbersome, and has division
-    // by zero problems
-    // (serpers method)
-    //segment seg1 = segment(p1, p2);
-    //segment seg2 = segment(p2, p3);
-    //point middle1 = seg1.middle();
-    //point middle2 = seg2.middle();
-    //point normal1 = seg1.normal();
-    //point normal2 = seg2.normal();
-    //center = intersect(middle1-normal1, middle1+normal1,
-    //                   middle2-normal2, middle2+normal2);
-    //radius = distance(center,p1);
-    long double M11 = a.x*b.y+c.x*a.y+b.x*c.y-a.x*b.y-b.x*a.y-a.x*c.y;
+    long double M11 = a.x*(b.y-c.y)+c.x*(a.y-b.y)+b.x*(c.y-a.y);
     long double sum0 = a.x*a.x+a.y*a.y;
     long double sum1 = b.x*b.x+b.y*b.y;
     long double sum2 = c.x*c.x+c.y*c.y;
-    long double M12 = sum0*b.y+sum2*a.y+sum1*c.y-sum2*b.y-sum2*a.y-sum0*c.y;
-    long double M13 = sum0*b.x+sum2*a.x+sum1*c.x-sum2*b.x-sum2*a.x-sum0*c.x;
+    long double M12 = sum0*b.y+sum2*a.y+sum1*c.y-sum2*b.y-sum1*a.y-sum0*c.y;
+    long double M13 = sum0*b.x+sum2*a.x+sum1*c.x-sum2*b.x-sum1*a.x-sum0*c.x;
     long double x0 =   0.5 * M12 / M11;
     long double y0 = - 0.5 * M13 / M11;
     point center = point(x0, y0);
@@ -156,40 +107,35 @@ circle min_disc2(vector<point>& points, int pos0, int pos1) {
     point p1 = points[pos1];
     circle circ(segment(p0,p1).middle(),
                 distance(p0,p1)/2);
-    cout << "min_disc2 init: " << circ << endl;
-    for (int i = 0; i < pos1; i++) {
+    cerr << "min_disc2 init: " << circ << endl;
+    for (int i = 0; i < min(pos1,pos0); i++) {
         if (!circ.inside(points[i])) {
             circ = from_3_points(points[i], p0, p1);
         }
     }
     return circ;
-    cout << "min_disc2 ret: " << circ << endl;
+    cerr << "min_disc2 ret: " << circ << endl;
 }
 
 circle min_disc1(vector<point>& points, int pos) {
     point p = points[pos];
     circle circ(segment(points[0],p).middle(),
                 distance(points[0],p)/2);
-    cout << "min_disc1 init: " << circ << endl;
     for (int i = 1; i < pos; i++) {
         if (!circ.inside(points[i])) {
             circ = min_disc2(points, pos, i);
         }
     }
     return circ;
-    cout << "min_disc1 ret: " << circ << endl;
 }
 
 circle min_disc0(vector<point>& points) {
     random_shuffle(points.begin(), points.end());
-    cout << points[0] << " " << points[1] << endl;
-    cout << segment(points[0],points[1]).middle() << endl;
+    if (points.size() == 1) return circle(points[0], 0);
     circle circ(segment(points[0],points[1]).middle(),
                 distance(points[0], points[1])/2);
-    cout << "min_disc0 init: " << circ << endl;
     for (int i = 2; i < points.size(); i++) {
         if (!circ.inside(points[i])) {
-            cout << "min_disc0_changing" << endl;
             circ = min_disc1(points, i);
         }
     }
@@ -214,11 +160,11 @@ int main() {
 
         circle ans = min_disc0(points);
 
-        fout << fixed << setprecision(9)
+        fout << fixed << setprecision(16)
              << ans.radius << endl
              << ans.center.x << " " << ans.center.y << endl;
 
-        cerr << fixed << setprecision(9)
+        cerr << fixed << setprecision(16)
              << ans.radius << endl
              << ans.center.x << " " << ans.center.y << endl;
 
